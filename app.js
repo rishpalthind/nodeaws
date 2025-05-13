43ee0c9b733c0c6a772816f087380e902b2f2c6c
rishpalthind <131610314+rishpalthind@users.noreply.github.com>
2025-05-13 11:22:16 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
+
+app.get('/ec2', (req, res) => {
+  res.send('Welcome to AWS EC2');
+});
 // Start the server
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
