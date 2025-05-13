2a656f9b6150544b9f2bbe3ebcf827cc81e315ea
Rishpal singh <rishpalthind@gmail.com>
2025-05-13 12:35:51 +0530

    update test

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.get('/', (req, res) => {
 
 
 app.get('/ec2', (req, res) => {
-  res.send('Welcome to AWS EC2');
+  res.send('Welcome to AWS EC2 Update dusing pipeline');
 });
 // Start the server
 app.listen(port, () => {
